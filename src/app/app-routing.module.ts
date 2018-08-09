38512a3d065a018f8e3b5adbf8cefb3e85f8a9da
Richard Mable <richardbmable@gmail.com>
2018-08-08 22:36:27 -0600

    added route to products list and individual products on app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ProductsComponent }   from './products/products.component';
+import { ProductDetailComponent }  from './product-detail/product-detail.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: 'products', component: ProductsComponent },
+  { path: 'product/:id', component: ProductDetailComponent },
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
