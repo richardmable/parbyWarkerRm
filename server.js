fd545e303b1c7acd79189d7782c4f30328440f87
Richard Mable <richardbmable@gmail.com>
2018-08-08 22:14:30 -0600

    moved the routes and models folder into the src folder

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import router from './routes';
+import router from './src/routes';
 
 let app = express();
 
